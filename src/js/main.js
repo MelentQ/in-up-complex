402943736694f5ecea2da0f4444e349ed71fed38
MelentQ <mejleht@yandex.ru>
2022-04-27 00:29:38 +0300

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+import polyfills from './polyfills';
+import './lazyload';
+import detectTouch from './detectTouch';
+import setScrollbarWidth from './setScrollbarWidth';
+import validation from './validation';
+import customSelects from './customSelects';
+import masks from './masks';
+import fileUpload from './fileUpload';
+import anchorLinks from './anchorLinks';
+import mediaPlayer from './mediaPlayer';
+import datepicker from './datepicker';
+import accordions from './accordions';
+import modals from './modals';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', function () {
+    polyfills();
+    detectTouch();
+    setScrollbarWidth();
+    validation();
+    customSelects();
+    masks();
+    fileUpload();
+    anchorLinks();
+    accordions();
+    mediaPlayer();
+    modals();
+    datepicker();
+
+});
+
+document.addEventListener('lazyloaded', () => {
+    ScrollTrigger.refresh();
+});
+
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+    ScrollTrigger.refresh();
+    setTimeout(() => document.body.classList.add('animatable'), 300);
+});
