371f7585cd039b5fe59b22f710766fa393b12c2e
MelentQ <mejleht@yandex.ru>
2022-04-27 22:48:53 +0300

    Карта + калькулятор

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,51 +4,131 @@ import noUiSlider from 'nouislider';
 export default function calculator() {
   const container = document.querySelector('.js-init-calculator');
   if (container) {
-    const checkbox = container.querySelector('.js-checkbox');
-    checkbox.addEventListener('click', () => {
-      checkbox.classList.toggle('active');
-    })
-
-    const sliderElement = container.querySelector('.swiper');
-    const swiper = new Swiper(sliderElement, {
-      slidesPerView: 5,
-      centeredSlides: true,
-      loop: true
-    });
-
-    const rangeElement = container.querySelector('.js-range');
-    const range = noUiSlider.create(rangeElement, {
-        range: {
-          'min': 1,
-          'max': 5
-        },
-        start: 1,
-        connect: false,
-        step: 1
-    });
     
-    const input = container.querySelector('.js-input');
-    range.on('update', (values) => {
-      input.value = Math.round(values[0]);
+    const url = container.dataset.url;
+
+    fetch(url)
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        render(data);
+      })
+
+    function render(data) {
+      let current = 'cat';
+
+      const slidesContainer = container.querySelector('.calculator__slider-wrapper');
+      data.boxes.forEach(item => {
+        const template = getTemplate(slidesContainer, "#slide");
+        const image = template.querySelector('img');
+        image.src = item.image;
+        image.alt = item.name;
+        const title = template.querySelector('.calculator__slide-title');
+        title.textContent = item.name;
+
+        slidesContainer.append(template);
+      })
+
+      const sliderElement = container.querySelector('.swiper');
+      const swiper = new Swiper(sliderElement, {
+        slidesPerView: 5,
+        centeredSlides: true,
+        loop: true,
+        allowTouchMove: false
+      });
+
+      let range; // no ui slider
+
+      const rangeLabel = container.querySelector('.range__label');
+
+      const checkbox = container.querySelector('.js-checkbox');
+      checkbox.addEventListener('click', () => {
+        checkbox.classList.toggle('active');
+
+        if (checkbox.classList.contains('active')) {
+          current = 'dog';
+
+          range.updateOptions({
+            range: {
+              'min': 1,
+              'max': 50
+            },
+            start: 10
+          })
+          swiper.slideToLoop(data.boxes.findIndex((item) => item.type === "dog"))
+        } else {
+          current = 'cat';
+          range.updateOptions({
+            range: {
+              'min': 1,
+              'max': 10
+            },
+            start: 2
+          })
+          swiper.slideToLoop(data.boxes.findIndex((item) => item.type === "cat"))
+        }
+      })
+
+      const rangeElement = container.querySelector('.js-range');
+      range = noUiSlider.create(rangeElement, {
+          range: {
+            'min': 1,
+            'max': 10
+          },
+          start: 2,
+          connect: false,
+          step: 1
+      });
+      
+      const input = container.querySelector('.js-input');
+      range.on('update', (values) => {
+        input.value = Math.round(values[0]);
+        swiper.slideToLoop(checkWeight(data, Number(input.value)));
+        checkDoses(data, input.value);
         setSize(input);
-    })
+      })
 
-    setSize(input);
-
-    input.addEventListener('change', () => {
-      range.set(input.value);
       setSize(input);
-    })
 
-    input.addEventListener('input', () => {
-      setSize(input);
-    })
+      input.addEventListener('change', () => {
+        range.set(input.value);
+        setSize(input);
+      })
 
-    function setSize(input) {
-      input.size = input.value.length || 1;
-      if (input.value.length > 3) {
-        input.size = 3;
+      input.addEventListener('input', () => {
+        setSize(input);
+      })
+
+      function setSize(input) {
+        input.size = input.value.length || 1;
+        if (input.value.length > 3) {
+          input.size = 3;
+        }
+      }
+
+      function checkWeight(data, weight) {
+        return data.boxes.findIndex(item => current === item.type && item.weight.from <= weight && item.weight.to >= weight) || 0;
+      }
+
+      function checkDoses(data, weight) {
+        const item = data.doses.find(item => current === item.type && item.weight.from <= weight && item.weight.to >= weight) || data.doses[0];
+
+        rangeLabel.textContent = `разовая доза ${item.drops} капель = ${item.dose} мл`
       }
     }
   }
+}
+
+/**
+ * Получает шаблон элемента
+ * @param {Object} container DOM-элемент, контейнер, в котором находится шаблон
+ * @param {String} selector CSS-селектор шаблона
+ * @returns {Object} DOM-элемент
+ */
+ function getTemplate(container = document, selector) {
+  return container.querySelector(selector)
+    .content
+    .children[0]
+    .cloneNode(true);
 }
--- src/js/map.js
+++ src/js/map.js
@@ -2,56 +2,89 @@ export default function map() {
   const container = document.querySelector('.js-init-map');
   if (container) {
 
-    // Dropdown
-    const dropdowns = Array.from(container.querySelectorAll('.dropdown'));
-    dropdowns.forEach(dropdown => {
-      const btn = dropdown.querySelector('.dropdown__btn');
+    const url = container.dataset.url;
 
-      btn.addEventListener('click', () => {
-        dropdown.classList.toggle('active');
+    fetch(url)
+      .then(res => {
+        return res.json()
       })
-
-      window.addEventListener('click', e => {
-        const target = e.target
-        if (!target.closest('.dropdown__content') && !target.closest('.dropdown__btn')) {
-          dropdown.classList.remove('active');
-        }
-      })
-
-      const options = dropdown.querySelectorAll('.dropdown__option');
-      const selected = dropdown.querySelector('.dropdown__selected');
-      options.forEach((option, i) => {
-        option.addEventListener('click', () => {
-          selected.textContent = option.textContent;
-          options.forEach((opt, j) => {
-            if (i === j) {
-              opt.classList.add('active');
-            } else {
-              opt.classList.remove('active');
-            }
-          })
-          dropdown.classList.remove('active');
+      .then(data => {
+        ymaps.ready(() => {
+          render(data);
         })
       })
-    })
 
-    // Map
-    ymaps.ready(init);
-    function init(){
-        const map = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 10,
-            controls: []
-        });
+    function render(data) {
+      // Map
+      const map = new ymaps.Map("map", {
+        center: data[0].coords,
+        zoom: 10,
+        controls: []
+      });
+      data.forEach(city => {
+        city.places.forEach(place => {
+          const placemark = new ymaps.Placemark(place.coords);
+          map.geoObjects.add(placemark);
+        })
+      })
 
-        const placemark = new ymaps.Placemark([55.8, 37.6]);
-        map.geoObjects.add(placemark);
+      // Dropdown
+      const dropdowns = Array.from(container.querySelectorAll('.dropdown'));
+      dropdowns.forEach(dropdown => {
+        const btn = dropdown.querySelector('.dropdown__btn');
 
-        const placemark2 = new ymaps.Placemark([55.9, 37.7]);
-        map.geoObjects.add(placemark2);
+        btn.addEventListener('click', () => {
+          dropdown.classList.toggle('active');
+        })
 
-        const placemark3 = new ymaps.Placemark([55.7, 37.5]);
-        map.geoObjects.add(placemark3);
+        window.addEventListener('click', e => {
+          const target = e.target
+          if (!target.closest('.dropdown__content') && !target.closest('.dropdown__btn')) {
+            dropdown.classList.remove('active');
+          }
+        })
+
+        const selected = dropdown.querySelector('.dropdown__selected');
+        selected.textContent = data[0].city;
+
+        const optionsContainer = dropdown.querySelector('.dropdown__options');
+
+        data.forEach(item => {
+          const option = getTemplate(optionsContainer, '#option');
+          option.textContent = item.city;
+
+          option.addEventListener('click', () => {
+            map.setCenter(item.coords, 10);
+
+            selected.textContent = option.textContent;
+            const options = dropdown.querySelectorAll('.dropdown__option');
+            options.forEach((opt, j) => {
+              opt.classList.remove('active');
+            })
+            option.classList.add('active');
+
+            dropdown.classList.remove('active');
+          })
+
+          optionsContainer.append(option);
+        })
+
+        const options = dropdown.querySelectorAll('.dropdown__option');
+        options[0].classList.add('active');
+      })
     }
   }
+}
+
+/**
+ * Получает шаблон элемента
+ * @param {Object} container DOM-элемент, контейнер, в котором находится шаблон
+ * @param {String} selector CSS-селектор шаблона
+ * @returns {Object} DOM-элемент
+ */
+ function getTemplate(container = document, selector) {
+  return container.querySelector(selector)
+    .content
+    .children[0]
+    .cloneNode(true);
 }
